d94f733c21a12f319d8911888c42e13ab2eb0a00
Marcel <marcel.johannesmann@gmx.de>
2021-03-15 21:14:04 +0100

    Added Java frame window to access the original of a measurement image.

--- autoJ.py
+++ autoJ.py
@@ -1,11 +1,12 @@
 # @ File(label='Choose a directory', style='directory') import_dir
 # @ String(label='File types', value='tif;png') file_types
-# @ String(label='Filter', value='GFP') filters
+# @ String(label='Filter for measure', value='GFP') filters_measure
+# @ String(label='Filter for prop', value='GFP') filters_original
 # @ Boolean(label='Recursive search', value=True) do_recursive
 # @ Integer(label='Measurements', value=2) measurements
 # @ Boolean(label='Export Measurements', value=True) do_export
 
-'''A batch based opener for ROI measurement in a series of images
+"""A batch based opener for ROI measurement in a series of images
 
 On start the user is asked for a directory containing the image series as well
 as optional arguments like file types or key words for filtering. After that
@@ -13,107 +14,139 @@ the first image is opened and a ROI has to be selected before the measurement
 is taken via a user dialog. This process is repeated for n measurements,
 defined in the first dialog window and all filter matching images.
 
----
+----------
+
 REQUIREMENTS:
-	ImageJ 1.39r or later, see:
-	https://imagejdocu.tudor.lu/plugin/utilities/wait_for_user/start#usage_in_a_plugin
 
----
+ImageJ 1.39r or later, because of the dependency of built-in WaitForUserDialog:
+https://imagejdocu.tudor.lu/plugin/utilities/wait_for_user/start#usage_in_a_plugin
+
+----------
+
 USAGE:
-    - EXECUTION
-    Open Fiji / ImageJ , then open this file in the onboard Script Editor via
-    dialog option FILE > OPEN... or by pressing CTRL + O.
-    Now RUN the script and the first dialog window will open.
-    During a run, the kill option is blocked due the gui elements.
-        To end the script execution click OK and then hold ESC to manually exit.
 
-    - DIALOG OPTIONS
-    Paste a path to a directory with an image series or browse for your data.
-    By default the last path and user inputs are shown. To modify this
-    behaviour replace the header with global constants e.g.:
-        # @ String(label='File types', value='tif;png') file_types
-        file_types = 'tif;png'
-    To filter your files and measure only specific images of your directory add
-    search keys (based on the images file name) as optional argument. For more
-    than one key use ; as separator (s.a.).
-    By default this script takes two measurements per image. Users may take more
-    values by adjusting this dialog option.
-    The export checkbox is implemented for further development...
-    By pressing ok the script searches for matching images and starts the
-    procedure.
+- EXECUTION
 
-    - MEASUREMENT
-    On start the script opens the first image and a dialog option. Later is
-    used to take a measurement and advance. This script uses simple ui
-    settings, therefore users may arrange windows first.
-        Note, that windows always open on the same monitor as ImageJ. After
-        arranging the gui layout, it will maintain over different executions.
-    Before proceeding by clicking OK, select a region of interest (ROI) to
-    measure. The script asks for n measurements before proceeding to the next
-    image.
-        If the image has no sufficient data, close it BEFORE advancing by
-        pressing OK. Otherwise a measurement is taken even though no ROI has
-        been selected (ROI is the whole image).
-        To open a new image, advance mit OK till it's shown.
-        For further study, skipped images can be found in the console output.
+Open Fiji / ImageJ , then open this file in the onboard Script Editor via
+dialog option FILE > OPEN... or by pressing CTRL + O.
+Now RUN the script and the first dialog window will open.
+During a run, the kill option is blocked due the gui elements.
+    To end the script execution click OK and then hold ESC to manually exit.
 
-    - RESULTS
-    The results of all measurements can be found in the ImageJ results
-    window. Users may export all values as a csv file.
-    An automatically export function is planned for further releases.
+- DIALOG OPTIONS
+Paste a path to a directory with an image series or browse for your data.
+By default the last path and user inputs are shown. To modify this
+behaviour replace the header with global constants e.g.,
+replace:    # @ String(label='File types', value='tif;png') file_types
+with:       file_types = 'tif;png'
+To filter your files and measure only specific images of your directory add
+search keys (based on the images file name) as optional argument. For more
+than one key use ; as separator (s.a.).
+By default this script takes two measurements per image. Users may take more
+values by adjusting this dialog option.
+The export checkbox is implemented for further development...
+By pressing ok the script searches for matching images and starts the
+procedure.
 
----
+- MEASUREMENT
+On start the script opens the first image and a dialog option. Later is
+used to take a measurement and advance. This script uses simple ui
+settings, therefore users may arrange windows first.
+    Note, that windows always open on the same monitor as ImageJ. After
+    arranging the gui layout, it will maintain over different executions.
+Before proceeding by clicking OK, select a region of interest (ROI) to
+measure. The script asks for n measurements before proceeding to the next
+image.
+    If the image has no sufficient data, close it BEFORE advancing by
+    pressing OK. Otherwise a measurement is taken even though no ROI has
+    been selected (ROI is the whole image).
+    To open a new image, advance mit OK till it's shown.
+    For further study, skipped images can be found in the console output.
+
+- RESULTS
+The results of all measurements can be found in the ImageJ results
+window. Users may export all values as a csv file.
+An automatically export function is planned for further releases.
+
+----------
+
 RESOURCES:
-	This script is mainly based on the template 'Wiki_Jython_Tutorial_3.py'
-	and the below mentioned resources.
 
-	imagej doc:		
-	https://imagej.nih.gov/ij/developer/api/overview-summary.html	
+This script is mainly based on the template 'Wiki_Jython_Tutorial_3.py'
+and the below mentioned resources.
 
-	imagej macro language:
-	https://imagej.nih.gov/ij/developer/macro/macros.html
-	
-	Fiji Scripting Tutorial:
-	https://syn.mrc-lmb.cam.ac.uk/acardona/fiji-tutorial/#directory-chooser
----
+imagej doc:
+https://imagej.nih.gov/ij/developer/api/overview-summary.html
+
+imagej macro language:
+https://imagej.nih.gov/ij/developer/macro/macros.html
+
+Fiji Scripting Tutorial:
+https://syn.mrc-lmb.cam.ac.uk/acardona/fiji-tutorial/
+
+----------
+
+AUTHORS COMMENT:
+
+During development dent mistakes have shown to be the greatest cause of
+errors. Therefore check dents containing a combination of spaces and tabs and
+unify the indent of error lines.
+The onboard script editor of ImageJ is a rudimentary development environment,
+but offers an automatically relevance control. Therefore development may be
+done in your favourite IDE with execution in ImageJ, after reloading the latest
+script version.
+Compared with the normal packing and import of python modules, the import of
+self written Jython modules is a bit more complex. To keep it simple this
+script consists of only one module and doesn't need any further
+installations. Simply open and run the file with ImageJ.
+Fur further information on Jython packaging see:
+https://imagej.net/Jython_Scripting
+
+----------
 
 TODO:
-	- add csv export via results: https://syn.mrc-lmb.cam.ac.uk/acardona/fiji-tutorial/#measurements-results-table
-	- open corresponding original via button, for further analysis of very
+	- add csv export via results:
+	https://syn.mrc-lmb.cam.ac.uk/acardona/fiji-tutorial/#measurements-results-table
+	- open corresponding prop via button, for further analysis of very
 	    close cells
-'''
+
+"""
 
 # We do only include the module os,
 # as we can use os.path.walk()
 # to access functions of the submodule.
 import os
+# java imports
 from java.io import File
-
-from ij import IJ
-from ij.gui import GenericDialog
-
 from javax.swing import JFrame, JButton, JOptionPane
-from ij.gui import WaitForUserDialog
+from java.awt.event import ActionListener
+# ImageJ imports
 from ij import IJ, WindowManager as WM
+from ij.gui import GenericDialog, WaitForUserDialog
 
 
 def batch_open_images(path, file_type=None, name_filter=None, recursive=False):
-    '''Open all files in the given folder.
-    :param path: The path from were to open the images. String and java.io.File are allowed.
-    :param file_type: Only accept files with the given extension (default: None).
-    :param name_filter: Only accept files that contain the given string (default: None).
-    :param recursive: Process directories recursively (default: False).
-    '''
+    """Open all files in the given folder.
+    :param path:        The path from were to open the images.
+                        String and java.io.File are allowed.
+    :param file_type:   Only accept files with the given extension
+                        (default: None).
+    :param name_filter: Only accept files that contain the given string
+                        (default: None).
+    :param recursive:   Process directories recursively
+                        (default: False).
+    """
     # Converting a File object to a string.
     if isinstance(path, File):
         path = path.getAbsolutePath()
 
     def check_type(string):
-        '''This function is used to check the file type.
-        It is possible to use a single string or a list/tuple of strings as filter.
-        This function can access the variables of the surrounding function.
+        """This function is used to check the file type.
+        It is possible to use a single string or a list/tuple of strings as
+        filter. This function can access the variables of the surrounding
+        function.
         :param string: The filename to perform the check on.
-        '''
+        """
         if file_type:
             # The first branch is used if file_type is a list or a tuple.
             if isinstance(file_type, (list, tuple)):
@@ -136,11 +169,11 @@ def batch_open_images(path, file_type=None, name_filter=None, recursive=False):
             return True
 
     def check_filter(string):
-        '''This function is used to check for a given filter.
+        """This function is used to check for a given filter.
         It is possible to use a single string or a list/tuple of strings as filter.
         This function can access the variables of the surrounding function.
         :param string: The filename to perform the filtering on.
-        '''
+        """
         if name_filter:
             # The first branch is used if name_filter is a list or a tuple.
             if isinstance(name_filter, (list, tuple)):
@@ -205,16 +238,16 @@ def batch_open_images(path, file_type=None, name_filter=None, recursive=False):
 
 
 def split_string(input_string):
-    '''Split a string to a list and strip it
+    """Split a string to a list and strip it
     :param input_string: A string that contains semicolons as separators.
-    '''
+    """
     string_splitted = input_string.split(';')
     # Remove whitespace at the beginning and end of each string
     strings_striped = [string.strip() for string in string_splitted]
     return strings_striped
 
 
-def measureBlock():
+def measure_block():
     # save image name before user choice
     image_name = str(WM.getCurrentImage())
     # 1st macro call, to wait for user action and avoid accessing a closed image
@@ -228,7 +261,7 @@ def measureBlock():
         print(image_name)
 
 
-def printConsoleNotes():
+def print_console_notes():
     # display notes on usage to console
     line = "________________________________________________________________" \
            "_________________________________________________ "
@@ -246,28 +279,77 @@ def printConsoleNotes():
         print(o)
 
 
-if __name__ in ['__builtin__', '__main__']:
+class ButtonClic(ActionListener):
+    """Class which unique function is to handle the button clicks
+    For accessibility this class is defined in the same file. It handles the
+    onclick event of a simple java window, to open the original image of a
+    measurement to simplify access during execution.
+    """
+    prop = None  # init property
+
+    def actionPerformed(self, event):
+        # implement abstract method 'actionPerformed' from
+        # 'java.awt.event.ActionListener'
+        print(self, self.prop)
+
+    def set_original(self, orig):
+        self.prop = orig
+
+
+def run_script():
+    # This enables us to stop the script by just calling return.
     # Run the batch_open_images() function using the Scripting Parameters.
     images = batch_open_images(import_dir,
                                split_string(file_types),
-                               split_string(filters),
+                               split_string(filters_measure),
                                do_recursive)
+
+    originals = batch_open_images(import_dir,
+                               split_string(file_types),
+                               split_string(filters_original),
+                               do_recursive)
+    if not images:
+        # exit script without matching images
+        print('No matching images could be found. Please check input arguments'
+              '\nScript was cancelled.')
+        return
+
     # note on runtime exit
-    printConsoleNotes()
+    print_console_notes()
+
+    # generate button to show corresponding original image
+    # first generate java window frame
+    frame = JFrame("Open Original Image", visible=True)
+    frame.setSize(500, 125)
+    # create button and onclick event s.a.
+    button = JButton("Open Original")
+    button_event = ButtonClic()
+    button.addActionListener(button_event)
+    # We associate the button objects to some instance of the ButtonClic class
+    frame.getContentPane().add(button)
+    frame.pack()
 
     # looping through matching images
-    for image in images:
+    for idx, image in enumerate(images):
         # IJ.log(str(image )) # to log file names
         # open method used to open different extension image file
         image.show()
+        button_event.set_original(idx)
         # calls measure function and logs results
         # by adjusting the inner loop, more measurements per image are possible
         for i in range(measurements):
             # measure ROIs per image
-            measureBlock()
+            measure_block()
         # close image after n measurements
         image.close()
+
+    # post processing:
     if do_export:
         print("here comes the export")
-
+    # close java window and end script with console message
+    frame.dispose()
     print("\n...finished autoJ.py")
+
+
+if __name__ in ['__builtin__', '__main__']:
+    run_script()
